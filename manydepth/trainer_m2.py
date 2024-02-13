7aee9ccf47e814a784c948fd2482d6062eae8d0b
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-02-13 16:36:44 -0600

    frame_id

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -560,7 +560,7 @@ class Trainer_Monodepth:
             #wandb.log({"Mask_{}_{}".format(frame_id, scale): wandb.Image(reprojection_loss_mask[0].data)},step=self.step)
             for frame_id in self.opt.frame_ids[1:]:
                 #Mask
-                target = inputs[("color_refined", 0, source_scale)]
+                target = inputs[("color_refined", frame_id, source_scale)]
                 pred = outputs[("color", frame_id, scale)]
                 
                 rep = self.compute_reprojection_loss(pred, target)
