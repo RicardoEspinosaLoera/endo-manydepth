7a227585a49f4f91f02911d98c6ec4fac39ba46b
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-02-21 16:16:54 -0600

    loss_ilumination_invariant

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -580,8 +580,8 @@ class Trainer_Monodepth:
                 loss_reprojection += (self.compute_reprojection_loss(pred, target) * reprojection_loss_mask).sum() / reprojection_loss_mask.sum()
                 
                 #Illuminations invariant loss
-                #target = inputs[("color", 0, 0)]
-                target = outputs[("color_refined", frame_id, scale)].detach()
+                target = inputs[("color", 0, 0)]
+                #target = outputs[("color_refined", frame_id, scale)].detach()
                 loss_ilumination_invariant += (self.get_ilumination_invariant_loss(pred,target) * reprojection_loss_mask_iil).sum() / reprojection_loss_mask_iil.sum()                
                 
                 
