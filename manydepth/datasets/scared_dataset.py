ceee9223b583a321d4970ea0d1279b4928f42e8a
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-02-26 18:53:08 -0600

    folder

--- manydepth/datasets/scared_dataset.py
+++ manydepth/datasets/scared_dataset.py
@@ -105,7 +105,7 @@ class SCAREDRAWDataset(SCAREDDataset):
         #image_path = os.path.join(self.data_path, folder, "data", f_str)
         
         #COLON10k
-        
+        print(folder)
         frame = folder.replace("depth","image").replace("/","_") + self.img_ext
         #print(frame)
         #f_str=str(frame_index) + self.img_ext
