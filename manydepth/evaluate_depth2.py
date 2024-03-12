b6a72d3f4ee01a066444c0114c1908eb1be995e7
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-03-12 17:14:25 -0600

    gt_depth = gt_depths[i] / 100.0

--- manydepth/evaluate_depth2.py
+++ manydepth/evaluate_depth2.py
@@ -215,13 +215,13 @@ def evaluate(opt):
 
     for i in range(pred_disps.shape[0]):
 
-        gt_depth = gt_depths[i]
-        """
+        gt_depth = gt_depths[i] / 100.0
+        
         ma = float(gt_depth.max())
         mi = float(gt_depth.min())
-        d = ma - mi if ma != mi else 1e5"""
+        d = ma - mi if ma != mi else 1e5
         #print(ma,mi,"---",i)
-        #gt_depth = (gt_depth - mi) / d
+        gt_depth = (gt_depth - mi) / d
         gt_height, gt_width = gt_depth.shape[:2]
         pred_disp = pred_disps[i]
         disp = colormap(pred_disp)
