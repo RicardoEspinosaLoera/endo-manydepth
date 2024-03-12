1d77ff85b8d5a96ffa31e6f7b334e2ca00219921
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-03-12 12:39:20 -0600

    normalize

--- manydepth/evaluate_depth2.py
+++ manydepth/evaluate_depth2.py
@@ -216,8 +216,10 @@ def evaluate(opt):
     for i in range(pred_disps.shape[0]):
 
         gt_depth = gt_depths[i]
-        #print(gt_depths[i].shape)
-        #print(gt_depths[i])
+        ma = float(gt_depth.max())
+        mi = float(gt_depth.min())
+        d = ma - mi if ma != mi else 1e5
+        gt_depth = (gt_depth - mi) / d
         gt_height, gt_width = gt_depth.shape[:2]
         pred_disp = pred_disps[i]
         disp = colormap(pred_disp)
