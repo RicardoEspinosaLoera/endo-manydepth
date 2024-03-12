71ec6df2400064ff52223b0b6c05c51438ee7829
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-03-11 20:14:39 -0600

    folder1

--- manydepth/datasets/scared_dataset.py
+++ manydepth/datasets/scared_dataset.py
@@ -86,13 +86,17 @@ class SCAREDDataset(MonoDataset):
 
     def get_image_path(self, folder, frame_index, side):
         #SCATER
+        """
         f_str = "{}{}".format(frame_index, self.img_ext)
-        image_path = os.path.join(self.data_path, folder, "data", f_str)
+        image_path = os.path.join(self.data_path, folder, "data", f_str)"""
         #COLON10k
 
         #f_str=str(frame_index) + self.img_ext
         #image_path = os.path.join(self.data_path, folder,"rgb", f_str)
-            
+        #Hamlyn
+        #f_str = "{}{}".format(frame_index, self.img_ext)
+        folder1,folder2 = folder.split("/")
+        image_path = os.path.join(self.data_path, folder1,folder1,folder2, f_str)        
         return image_path
 
 class SCAREDRAWDataset(SCAREDDataset):
