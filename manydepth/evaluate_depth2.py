5ac0105cb35790bdee8a9efdf954ff7a089c3577
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-04-02 10:54:01 -0600

    eval hamlyn

--- manydepth/evaluate_depth2.py
+++ manydepth/evaluate_depth2.py
@@ -216,11 +216,7 @@ def evaluate(opt):
     for i in range(pred_disps.shape[0]):
 
         gt_depth = gt_depths[i]
-        ma = float(gt_depth.max())
-        mi = float(gt_depth.min())
-        d = ma - mi if ma != mi else 1e5
-        print(ma,mi,"---",i)
-        gt_depth = (gt_depth - mi) / d
+        
         gt_height, gt_width = gt_depth.shape[:2]
         pred_disp = pred_disps[i]
         disp = colormap(pred_disp)
