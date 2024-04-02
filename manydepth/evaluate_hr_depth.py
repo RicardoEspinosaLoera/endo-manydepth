eb905e750ef9aa1db7fef37810a6a9e5f3ff249e
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-04-02 11:25:49 -0600

    gt_depth

--- manydepth/evaluate_hr_depth.py
+++ manydepth/evaluate_hr_depth.py
@@ -207,13 +207,6 @@ def evaluate(opt):
     for i in range(pred_disps.shape[0]):
 
         gt_depth = gt_depths[i]
-        gt_height, gt_width = gt_depth.shape[:2]
-        
-        ma = float(gt_depth.max())
-        mi = float(gt_depth.min())
-        d = ma - mi if ma != mi else 1e5
-        #print(ma,mi,"---",i)
-        gt_depth = (gt_depth - mi) / d
 
         pred_disp = pred_disps[i]
         disp = colormap(pred_disp)
