e8b12bffc6d1d17fd1e1fe185e801a5a7530fa74
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-05-02 22:46:37 -0600

    data_path

--- manydepth/export_gt_depth.py
+++ manydepth/export_gt_depth.py
@@ -53,9 +53,9 @@ def export_gt_depths_kitti():
             gt_depth = np.array(pil.open(gt_depth_path)).astype(np.float32) / 256"""
         print(line)
         folder, file,_ = line.split()
-        folder = folder.split("/")[0]
+        #folder = folder.split("/")[0]
         #line = line.replace("/","_")[1:]
-        gt_depth_path = os.path.join(opt.data_path,folder,"SERV-CT","Ground_truth_CT","DepthL", "{}.png".format(file))
+        gt_depth_path = os.path.join(opt.data_path,"SERV-CT",folder,"Ground_truth_CT","DepthL", "{}.png".format(file))
         print(gt_depth_path)
         #gt_depth = np.array(pil.open(gt_depth_path))
         #im = pil.open(gt_depth_path)
