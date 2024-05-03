84e01e6f58fb65e042ac0dea9fc5eae23abdb2fc
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-05-02 19:18:53 -0600

    hamlyn

--- manydepth/datasets/scared_dataset.py
+++ manydepth/datasets/scared_dataset.py
@@ -95,8 +95,9 @@ class SCAREDDataset(MonoDataset):
 
         #Hamlyn
         f_str = "{}{}".format(frame_index, self.img_ext)
-        folder1,folder2 = folder.split("/")
-        image_path = os.path.join(self.data_path, folder1,folder1,folder2, f_str)
+        
+        #folder1,folder2 = folder.split("/")
+        image_path = os.path.join(self.data_path,folder, f_str)
         
 
         return image_path
