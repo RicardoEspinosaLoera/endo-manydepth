778e2c17c357daf8a008075446a2c5805450d3a4
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-05-02 21:44:59 -0600

    monovit

--- manydepth/evaluate_hr_depth.py
+++ manydepth/evaluate_hr_depth.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 
 import wandb
 
-wandb.init(project="MonoViT-Testing", entity="respinosa")
+wandb.init(project="iilDepth-Testing", entity="respinosa")
 
 _DEPTH_COLORMAP = plt.get_cmap('plasma', 256)  # for plotting
 
