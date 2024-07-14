5ede0938a88b20151c9826f6baeecf6aec2ad531
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-07-13 18:37:06 -0600

    compute_orth_loss4

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -593,7 +593,7 @@ class Trainer_Monodepth2:
             orth_loss += torch.abs(D_inv * Cpq - Ds[d_names[idx]] * Cpp)
         return orth_loss.sum()
 
-    def compute_orth_loss4(self, disp, N_hat, K_inv, I):
+    def compute_orth_loss4(self, disp, N_hat, K_inv):
         orth_loss = 0
         _, D = disp_to_depth(disp, self.opt.min_depth, self.opt.max_depth)
         D_inv = 1.0 / D
@@ -1059,7 +1059,7 @@ class Trainer_Monodepth2:
 
         
         total_loss /= self.num_scales
-        total_loss += 0.5 * self.compute_orth_loss(outputs[("disp", 0)], outputs["normal_inputs"][("normal", 0)], inputs[("inv_K", 0)])
+        total_loss += 0.5 * self.compute_orth_loss4(outputs[("disp", 0)], outputs["normal_inputs"][("normal", 0)], inputs[("inv_K", 0)])
         #total_loss += 0.6 * self.compute_orth_loss5(outputs[("disp", 0)], outputs["normal_inputs"][("normal", 0)], inputs[("inv_K", 0)])
         losses["loss"] = total_loss
         
