4ed0a3e44df1205bcedd9f77e3685f48adda4527
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-07-25 21:26:41 -0600

    cpu

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -914,8 +914,11 @@ class Trainer_Monodepth2:
    
     def visualize_normal_image(self, xyz_image):
        
+        # Ensure the input tensor is on the CPU and in numpy format
+        #normal_image_np = xyz_image.cpu().permute(1, 2, 0).numpy()
+        
         # Convert from (3, H, W) to (H, W, 3)
-        surface_normals = np.transpose(xyz_image, (1, 2, 0))
+        surface_normals = np.transpose(xyz_image.cpu(), (1, 2, 0))
 
         # Normalize the surface normals to [0, 1] for visualization
         normalized_normals = (surface_normals + 1) / 2
@@ -923,8 +926,7 @@ class Trainer_Monodepth2:
         # Clip to ensure no values are outside [0, 1] due to floating point errors
         normalized_normals = np.clip(normalized_normals, 0, 1)
 
-
-        return normalized_normals  
+        return scaled_normals  
 
         
     def norm_to_rgb(self,norm):
