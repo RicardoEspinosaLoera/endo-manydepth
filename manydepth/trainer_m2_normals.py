87d1379eb614ecc5f92f892104bdef7f377af504
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-07-25 21:47:56 -0600

    prints

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -580,8 +580,6 @@ class Trainer_Monodepth2:
         N_hat = N_hat[:, :, :H-1, :W-1]
         N_hat = N_hat.repeat(2, 1, 1, 1)  # Shape (2*B, 3, H-1, W-1)
 
-        print("N_hat",N_hat.shape)
-        print("V_hat",V_hat.shape)
         # Compute the orthogonality loss
         loss = (N_hat * V_hat).sum(dim=1).mean()
         return loss
@@ -651,7 +649,9 @@ class Trainer_Monodepth2:
         # Reshape normal_pred to match V_hat
         N_hat = N_hat[:, :, :height-1, :width-1]
         N_hat = N_hat.repeat(2, 1, 1, 1)  # Shape (2*B, 3, H-1, W-1)
-
+        
+        print("N_hat",N_hat.shape)
+        print("V_hat",V_hat.shape)
         # Compute the orthogonality loss
         loss = (N_hat * V_hat).sum(dim=1).mean()
     
