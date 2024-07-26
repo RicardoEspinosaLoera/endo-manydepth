af1ec591a25ee2aece05f9a4d06b0a128c41ad7d
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-07-25 23:03:50 -0600

    visualize_normal_image

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -924,7 +924,12 @@ class Trainer_Monodepth2:
         # Convert from (3, H, W) to (H, W, 3)
         surface_normals = xyz_image.cpu().permute(1, 2, 0).numpy()
         # 2. predicted normal
-        pred_norm_rgb[:,:,:1] = ((surface_normals + 1) * 0.5) * 255
+        r =  ((surface_normals[:,:,0] + 1) * 0.5) * 255
+        g =  ((surface_normals[:,:,0] + 1) * 0.5) * 255
+        b =  (surface_normals[:,:,2] * 255)
+
+        pred_norm_rgb = np.dstack((r,g,b))
+        #pred_norm_rgb = ((surface_normals + 1) * 0.5) * 255
         pred_norm_rgb = np.clip(pred_norm_rgb, a_min=0, a_max=255)
         pred_norm_rgb = pred_norm_rgb.astype(np.uint8)                  # (B, H, W, 3)
 
