db91e9eae9af7e25885a3d9a3e47916ae4193f02
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-07-27 18:43:47 -0600

    R_ts

--- manydepth/trainer_m2_normals.py
+++ manydepth/trainer_m2_normals.py
@@ -515,9 +515,9 @@ class Trainer_Monodepth2:
         N_t_normalized = target / (torch.norm(target, dim=1, keepdim=True) + 1e-8)
 
         N_t_normalized = N_t_normalized.view(-1, 3, 1)  # Flatten (12, 256, 320, 3) to (983040, 3, 1)
-        R_ts_expanded = R_ts.repeat(256 * 320, 1, 1)  # Repeat R_ts for each pixel, (983040, 3, 3)
+        R_ts_expanded = R_ts[:,:3,:3].repeat(256 * 320, 1, 1)  # Repeat R_ts for each pixel, (983040, 3, 3)
         #print(R_ts_expanded.shape)
-        N_t_rotated = torch.bmm(R_ts_expanded[:,3,3], N_t_normalized)  # (983040, 3, 1)
+        N_t_rotated = torch.bmm(R_ts_expanded, N_t_normalized)  # (983040, 3, 1)
         N_t_rotated = N_t_rotated.view(12, 256, 320, 3)  # Reshape back to (12, 256, 320, 3)
 
         # Compute the L1 loss
