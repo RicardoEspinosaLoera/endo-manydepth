2b49c4202d6ffe5c6161baa6f5c3232c6d9832e4
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-07-29 19:16:16 -0600

    NormalDecoderT

--- manydepth/networks/__init__.py
+++ manydepth/networks/__init__.py
@@ -1,6 +1,7 @@
 from .resnet_encoder import ResnetEncoder, ResnetEncoderMatching
 from .depth_decoder import DepthDecoder
 from .hr_decoder import DepthDecoderT
+from .hr_decoder_normals import NormalDecoderT
 from .pose_decoder import PoseDecoder
 from .pose_cnn import PoseCNN
 from .lighting_decoder import LightingDecoder
