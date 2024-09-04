b335575688b2a83d38b3e0708b5a69df9dd7190d
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-09-04 17:57:45 -0600

    epsilon

--- manydepth/layers.py
+++ manydepth/layers.py
@@ -360,7 +360,8 @@ def get_ilumination_invariant_features(img):
     #t = torch.cat((M1/nor,M2/nor,M3/nor,M4/nor,M5/nor,M6/nor,M7/nor,M8/nor), dim = 1)
     t = torch.cat((M1,M2,M3,M4,M5,M6,M7,M8), dim = 1)
     #print(t.shape)
-    t_norm = t / (torch.sqrt((t ** 2).sum(dim=1, keepdim=True)) + 1e-9) 
+    epsilon = 1e-9
+    t_norm = torch.sqrt((t ** 2).sum(dim=1, keepdim=True)) + epsilon
     #t_norm = torch.norm(t)
     #print(t_norm.shape)
     #print(t.shape)
