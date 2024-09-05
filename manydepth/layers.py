8a59200193ee347a598206f8db99745c30e43bf1
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-09-04 18:08:54 -0600

    get_ilumination_invariant_features

--- manydepth/layers.py
+++ manydepth/layers.py
@@ -360,20 +360,19 @@ def get_ilumination_invariant_features(img):
     #t = torch.cat((M1/nor,M2/nor,M3/nor,M4/nor,M5/nor,M6/nor,M7/nor,M8/nor), dim = 1)
     t = torch.cat((M1,M2,M3,M4,M5,M6,M7,M8), dim = 1)
     #print(t.shape)
-    epsilon = 1e-9
-    t_norm = torch.sqrt((t ** 2).sum(dim=1, keepdim=True)) + epsilon
+    #t_norm = F.normalize(t, p=2, dim=1)
     #t_norm = torch.norm(t)
     #print(t_norm.shape)
     #print(t.shape)
-    return t_norm        
+    return t        
 
 def get_feature_oclution_mask(img):
     kernel = torch.tensor([[1, 1, 1],[1, 1, 1],[1, 1, 1]]).to(device=img.device).type(torch.cuda.FloatTensor)
     padding = (3 - 1) // 2  # Padding to maintain input size
     o = F.conv2d(img, kernel.view(1, 1, 3, 3), padding=padding)
     t = torch.cat((o,o,o,o,o,o,o,o), dim = 1)
-    t_norm = t / (torch.sqrt((t ** 2).sum(dim=1, keepdim=True)) + 1e-9) 
-    return t_norm
+    #t_norm = F.normalize(t, p=2, dim=1)
+    return t
 
 def calculate_surface_normal_from_depth(depth_map, K):
     # Calculate gradients using finite differences
