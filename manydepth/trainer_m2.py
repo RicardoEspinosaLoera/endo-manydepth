efc3eb6690bec44360eac15025d03163a11f3fc7
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-11-01 17:24:34 -0600

    loss_ilumination_invariant

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -574,12 +574,12 @@ class Trainer_Monodepth:
                 #Multiscale SIMM
                 #loss_reprojection += (self.get_ms_simm_loss(pred, target) * reprojection_loss_mask).sum() / reprojection_loss_mask.sum()
                 #Illuminations invariant loss
-                #target = inputs[("color", 0, 0)]
-                #loss_ilumination_invariant += (self.get_ilumination_invariant_loss(pred,target) * reprojection_loss_mask_iil).sum() / reprojection_loss_mask_iil.sum()
+                target = inputs[("color", 0, 0)]
+                loss_ilumination_invariant += (self.get_ilumination_invariant_loss(pred,target) * reprojection_loss_mask_iil).sum() / reprojection_loss_mask_iil.sum()
  
             
             loss += loss_reprojection / 2.0
-            #loss += self.opt.illumination_invariant * loss_ilumination_invariant / 2.0
+            loss += self.opt.illumination_invariant * loss_ilumination_invariant / 2.0
             mean_disp = disp.mean(2, True).mean(3, True)
             norm_disp = disp / (mean_disp + 1e-7)
             smooth_loss = get_smooth_loss(norm_disp, color)
