73962d76bb23a651cf2f81c848de807384970b17
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-11-01 23:43:19 -0600

    boolean_mask

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -603,7 +603,7 @@ class Trainer_Monodepth:
 
 
             #print(boolean_mask.shape)
-            reprojection_loss_mask = (reprojection_loss_mask * boolean_mask*-1)
+            reprojection_loss_mask = (reprojection_loss_mask * boolean_mask)
             
             
         return reprojection_loss_mask
