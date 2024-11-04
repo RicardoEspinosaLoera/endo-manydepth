7fd646bd5f73fd6e2b19336dc07779103cd20f90
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-11-04 11:53:03 -0600

    batch_size

--- manydepth/options.py
+++ manydepth/options.py
@@ -107,7 +107,7 @@ class MonodepthOptions:
         self.parser.add_argument("--batch_size",
                                  type=int,
                                  help="batch size",
-                                 default=12)
+                                 default=10)
         self.parser.add_argument("--learning_rate",
                                  type=float,
                                  help="learning rate",
