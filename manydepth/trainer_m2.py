102ea6abc36cd2d3d17c58f860e355c50c08b57f
RicardoEspinosaLoera <globbamexbustum@gmail.com>
2025-02-12 19:51:50 -0600

    color_refined

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -352,11 +352,12 @@ class Trainer_Monodepth:
                         
             #Lighting
             for f_i in self.opt.frame_ids[1:]:
+            #for i, frame_id in enumerate(self.opt.frame_ids[1:]):
                 for scale in self.opt.scales:
                     outputs[("bh",scale, f_i)] = F.interpolate(outputs["b_"+str(scale)+"_"+str(f_i)], [self.opt.height, self.opt.width], mode="bilinear", align_corners=False)
                     outputs[("ch",scale, f_i)] = F.interpolate(outputs["c_"+str(scale)+"_"+str(f_i)], [self.opt.height, self.opt.width], mode="bilinear", align_corners=False)
                     #outputs[("color_refined", f_i, scale)] = outputs[("ch",scale, f_i)] * inputs[("color", 0, 0)] + outputs[("bh", scale, f_i)]
-                    outputs[("color_refined", f_i, scale)] = outputs[("ch",scale, f_i)] * outputs[("color", f_i, scale)] + outputs[("bh", scale, f_i)]
+                    #outputs[("color_refined", f_i, scale)] = outputs[("ch",scale, f_i)] * outputs[("color", f_i, scale)] + outputs[("bh", scale, f_i)]
                     
 
 
@@ -431,6 +432,8 @@ class Trainer_Monodepth:
                     inputs[("color", frame_id, source_scale)],
                     outputs[("sample", frame_id, scale)],
                     padding_mode="border",align_corners=True)
+                
+                outputs[("color_refined", f_i, scale)] = outputs[("ch",scale, frame_id)] * outputs[("color", frame_id, scale)] + outputs[("bh", scale, frame_id)]
 
 
 
@@ -527,8 +530,8 @@ class Trainer_Monodepth:
                 reprojection_loss_mask = self.compute_loss_masks(rep,rep_identity,target)
                 reprojection_loss_mask_iil = get_feature_oclution_mask(reprojection_loss_mask)
                 #Losses
-                target = outputs[("color_refined", frame_id, scale)] #Lighting
-                pred = outputs[("color", frame_id, scale)]
+                #target = outputs[("color_refined", frame_id, scale)] #Lighting
+                pred = outputs[("color_refined", frame_id, scale)]
                 #SIMM
                 loss_reprojection += (self.compute_reprojection_loss(pred, target) * reprojection_loss_mask).sum() / reprojection_loss_mask.sum()
                 #Multiscale SIMM
