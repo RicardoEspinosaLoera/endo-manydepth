da1ed7de9880a56a0c5335ba03ffdd1e52394cbd
RicardoEspinosaLoera <globbamexbustum@gmail.com>
2025-02-18 23:02:38 -0600

    AdamW

--- manydepth/trainer_m2.py
+++ manydepth/trainer_m2.py
@@ -126,7 +126,7 @@ class Trainer_Monodepth:
             self.parameters_to_train += list(self.models["pose"].parameters())
 
 
-        self.model_optimizer = optim.Adam(self.parameters_to_train, self.opt.learning_rate)
+        self.model_optimizer = optim.AdamW(self.parameters_to_train, self.opt.learning_rate)
         """self.model_lr_scheduler = optim.lr_scheduler.StepLR(
             self.model_optimizer, self.opt.scheduler_step_size, 0.1)"""
         self.model_lr_scheduler = optim.lr_scheduler.ExponentialLR(
