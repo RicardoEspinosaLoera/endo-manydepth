0dde8846e26292d6a76a50ba96899f6496a0f1d5
RicardoEspinosaLoera <globbamexbustum@gmail.com>
2025-02-24 20:33:48 -0600

    SERV-CT

--- manydepth/datasets/scared_dataset.py
+++ manydepth/datasets/scared_dataset.py
@@ -127,7 +127,7 @@ class SCAREDRAWDataset(SCAREDDataset):
         #image_path = os.path.join(self.data_path,folder, f_str)
         #Serv-CT
         f_str = "{}{}".format(frame_index, self.img_ext)
-        image_path = os.path.join(self.data_path,folder, f_str)
+        image_path = os.path.join(self.data_path,"SERV-CT",folder, f_str)
         return image_path
 
     def get_depth(self, folder, frame_index, side, do_flip):
