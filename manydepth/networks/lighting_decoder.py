078d0693c30c113d9a834387c821e4fcab83480e
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2025-10-24 14:59:40 -0400

    lighting_decoder

--- manydepth/networks/lighting_decoder.py
+++ manydepth/networks/lighting_decoder.py
@@ -1,8 +1,6 @@
 # Copyright Niantic 2019. Patent Pending. All rights reserved.
-#
-# This software is licensed under the terms of the Monodepth2 licence
-# which allows for non-commercial use only, the full terms of which are made
-# available in the LICENSE file.
+# Licensed for non-commercial use under the Monodepth2 license.
+
 from __future__ import absolute_import, division, print_function
 
 import numpy as np
@@ -10,23 +8,30 @@ import torch
 import torch.nn as nn
 
 from collections import OrderedDict
-from layers import *
-
+from layers import *  # expects ConvBlock, Conv3x3, upsample
 
 class LightingDecoder(nn.Module):
-    def __init__(self, num_ch_enc, scales= range(4), num_output_channels=2, use_skips=False):
+    """
+    Lighting decoder with TWO HEADS:
+      - contrast head: predicts contrast scale (>= 0), activated with ReLU
+      - brightness head: predicts brightness shift in [-1, 1], activated with Tanh
+
+    Returns outputs at requested `scales` under keys:
+      ("contrast", s)   : [B, 1, H_s, W_s]
+      ("brightness", s) : [B, 1, H_s, W_s]
+    """
+    def __init__(self, num_ch_enc, scales=range(4), use_skips=False):
         super(LightingDecoder, self).__init__()
 
-        self.num_output_channels = num_output_channels
         self.use_skips = use_skips
         self.upsample_mode = 'nearest'
-        self.scales = scales
+        self.scales = list(scales)
 
         self.num_ch_enc = num_ch_enc
         self.num_ch_dec = np.array([16, 32, 64, 128, 256])
 
-        # decoder
-        self.convs = OrderedDict() # 有序字典
+        # ---------- shared decoder trunk ----------
+        self.convs = OrderedDict()
         for i in range(4, -1, -1):
             # upconv_0
             num_ch_in = self.num_ch_enc[-1] if i == 4 else self.num_ch_dec[i + 1]
@@ -40,38 +45,41 @@ class LightingDecoder(nn.Module):
             num_ch_out = self.num_ch_dec[i]
             self.convs[("upconv", i, 1)] = ConvBlock(num_ch_in, num_ch_out)
 
+        # ---------- per-scale HEADS ----------
+        # separate 1-channel conv heads for contrast and brightness
         for s in self.scales:
-            self.convs[("lighting_conv", s)] = Conv3x3(self.num_ch_dec[s], self.num_output_channels)
+            self.convs[("contrast_conv", s)] = Conv3x3(self.num_ch_dec[s], 1)
+            self.convs[("brightness_conv", s)] = Conv3x3(self.num_ch_dec[s], 1)
 
         self.decoder = nn.ModuleList(list(self.convs.values()))
-        #self.sigmoid = nn.Sigmoid()
+
+        # activations
+        self.act_contrast = nn.ReLU(inplace=True)
+        self.act_brightness = nn.Tanh()
 
     def forward(self, input_features):
-        self.outputs = {}
-        
-        # decoder
+        outputs = {}
+
+        # shared decoder
         x = input_features[-1]
-        #y = input_features[-1]
         for i in range(4, -1, -1):
             x = self.convs[("upconv", i, 0)](x)
-            #y = self.convs[("upconv", i, 0)](y)
-            x = [upsample(x)]
-            #y = [upsample(y)]
+            x = [upsample(x, mode=self.upsample_mode)]
             if self.use_skips and i > 0:
                 x += [input_features[i - 1]]
             x = torch.cat(x, 1)
-            #y = torch.cat(y, 1)
             x = self.convs[("upconv", i, 1)](x)
-            #y = self.convs[("upconv", i, 1)](y)
+
+            # heads at this scale
             if i in self.scales:
-                self.outputs[("lighting", i)] = self.convs[("lighting_conv", i)](x)
-                #self.outputs[("constrast", i)] = self.convs[("lighting_conv", i)](y)
-                # Split the output into C_t and B_t
-                Ct = torch.relu(self.outputs[("lighting", i)][:, 0:1, :, :])  # Contrast (scale)
-                Bt = torch.tanh(self.outputs[("lighting", i)][:, 1:2, :, :])  # Brightness (shift)
+                c = self.convs[("contrast_conv", i)](x)
+                b = self.convs[("brightness_conv", i)](x)
 
-                # Store outputs
-                self.outputs[("contrast", i)] = Ct
-                self.outputs[("brightness", i)] = Bt
+                # apply head-specific activations
+                c = self.act_contrast(c)     # >= 0
+                b = self.act_brightness(b)   # [-1, 1]
 
-        return self.outputs
+                outputs[("contrast", i)] = c
+                outputs[("brightness", i)] = b
+
+        return outputs
