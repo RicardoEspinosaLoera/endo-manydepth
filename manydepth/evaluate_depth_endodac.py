81ef72ddb4c62a20ce78aabdf1f0af13aba6e7e5
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2025-10-27 19:36:25 -0400

    compute_errors

--- manydepth/evaluate_depth_endodac.py
+++ manydepth/evaluate_depth_endodac.py
@@ -30,6 +30,26 @@ def render_depth(disp):
     disp_color = cv2.applyColorMap(disp, cv2.COLORMAP_INFERNO)
     return disp_color
 
+def compute_errors(gt, pred):
+    """Computation of error metrics between predicted and ground truth depths
+    """
+    thresh = np.maximum((gt / pred), (pred / gt))
+    a1 = (thresh < 1.25     ).mean()
+    a2 = (thresh < 1.25 ** 2).mean()
+    a3 = (thresh < 1.25 ** 3).mean()
+
+    rmse = (gt - pred) ** 2
+    rmse = np.sqrt(rmse.mean())
+
+    rmse_log = (np.log(gt) - np.log(pred)) ** 2
+    rmse_log = np.sqrt(rmse_log.mean())
+
+    abs_rel = np.mean(np.abs(gt - pred) / gt)
+
+    sq_rel = np.mean(((gt - pred) ** 2) / gt)
+
+    return abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3
+
 
 def batch_post_process_disparity(l_disp, r_disp):
     """Apply the disparity post-processing method as introduced in Monodepthv1
