582cc07c6a5a734c6e68e4bbdbe4ba7acf6037da
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2025-10-27 21:56:17 -0400

    testing

--- manydepth/datasets/mono_dataset.py
+++ manydepth/datasets/mono_dataset.py
@@ -151,9 +151,9 @@ class MonoDataset(data.Dataset):
                     other_side = {"r": "l", "l": "r"}[side]
                     inputs[("color", i, -1)] = self.get_color(folder, frame_index, other_side, do_flip)
                 else:
-                    #inputs[("color", i, -1)] = self.get_color(folder, frame_index + i, side, do_flip)
+                    inputs[("color", i, -1)] = self.get_color(folder, frame_index + i, side, do_flip)
                     #Hamlyn
-                    inputs[("color", i, -1)] = self.get_color(folder, frame_index, side, do_flip)
+                    #inputs[("color", i, -1)] = self.get_color(folder, frame_index, side, do_flip)
                     
                 """
                 if i == "s":
